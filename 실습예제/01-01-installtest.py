d1c7405eab788045b40cb758f9fcd63dcf3a3eb5
teamblender-group <82018368+teamblender-group@users.noreply.github.com>
2021-04-06 16:28:10 +0900

    Update 01-01-installtest.py

--- 실습예제/01-01-installtest.py
+++ 실습예제/01-01-installtest.py
@@ -1,1 +1,19 @@
-ㄹㅇㄴ
+import machine
+import utime
+
+LEDR = machine.Pin(17, machine.Pin.OUT)
+LEDG = machine.Pin(16, machine.Pin.OUT)
+LEDB = machine.Pin(27, machine.Pin.OUT)
+
+while True:
+  LEDR.value(1)
+  utime.sleep_ms(20)
+  LEDR.value(0)
+  
+  LEDG.value(1)
+  utime.sleep_ms(20)
+  LEDG.value(0)
+  
+  LEDG.value(1)
+  utime.sleep_ms(20)
+  LEDG.value(0)
